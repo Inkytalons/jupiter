e76208decb966077a6beb8086524a29a5597b52a
Inkytalons <fadeyidan8@gmail.com>
2025-10-15 06:35:34 +0100

    Add animation to content and error message.
    Add animation to search icon; make it rotate while data is fetched.
    Disable search button while data is being fetched to prevent conflict.
    Add animation to main weather card; make it gradually slide up rather than just appear.
    Make "Looking ahead" card fade in roughly 3 secs after data is fetched/main weather card is displayed.
    Change background colour of the main page to a gradient shifting from slate-500 to slate-700.

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,15 @@ import { useState } from "react";
 import { WeatherData } from "@/types/weather";
 import { Card, CardContent } from "@/components/ui/card";
 import { ForecastData, getDay } from "@/types/forecast";
+import { useFormStatus } from "react-dom";
+import { motion } from "framer-motion";
 
 function SubmitButton() {
+  const { pending } = useFormStatus();
+
   return (
-    <Button type="submit">
-      <Search className="w-4 h-4" />
+    <Button type="submit" disabled={pending}>
+      <Search className={`w-4 h-4 ${pending ? "animate-spin" : ""}`} />
     </Button>
   )
 }
@@ -47,7 +51,7 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-sky-400 to-blue-500 p-4">
+    <div className="min-h-screen bg-gradient-to-b from-slate-500 to-slate-700 p-4">
       <div className="flex text-3xl text-neutral-50 font-bold item-center justify-center my-10"> Is Jupiter on your side today? </div>
       <div className="flex items-center justify-center">
         <div className="w-full max-w-md space-y-4">
@@ -63,11 +67,23 @@ export default function Home() {
           </form>
 
           {error && (
-            <div className="text-center text-neutral-50 border border-neutral-50 bg-zinc-600/50 rounded-md p-2"> {error} </div>
+            <motion.div
+              initial={{ opacity: 0, y: 10}}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.7 }}
+              className="text-center text-neutral-50 border border-neutral-50 bg-zinc-600/50 rounded-md p-2"> 
+              {error}
+            </motion.div>
           )}
           
           {weather && forecast && (
-            <div>
+            <motion.div
+              initial={{ opacity: 0, y: 10}}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.7 }}
+            >
               <Card className="bg-white/70 backdrop-blur">
                 <CardContent className="p-6">
                   <div className="text-center mb-4">
@@ -106,57 +122,64 @@ export default function Home() {
                 </CardContent>
               </Card>
 
-              <Card className="bg-white/70 backdrop-blur mt-4">
-                <CardContent className="p-6">
-                {/*
-                  <div> At {forecast.list[0].dt_txt} it is {forecast.list[0].main.temp}°C</div>
-                  <div> At {forecast.list[1].dt_txt} it is {forecast.list[1].main.temp}°C</div>
-                  <div> At {forecast.list[2].dt_txt} it is {forecast.list[2].main.temp}°C</div>
-                */}
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.7, delay: 3 }}
+              >
+                <Card className="bg-white/70 backdrop-blur mt-4">
+                  <CardContent className="p-6">
+                  {/*
+                    <div> At {forecast.list[0].dt_txt} it is {forecast.list[0].main.temp}°C</div>
+                    <div> At {forecast.list[1].dt_txt} it is {forecast.list[1].main.temp}°C</div>
+                    <div> At {forecast.list[2].dt_txt} it is {forecast.list[2].main.temp}°C</div>
+                  */}
 
 
-                <div className="text-center font-bold text-2xl mb-6">Looking ahead...</div>
-                <div className="mb-2 border-b border-gray-500">
-                  <div className="font-semibold">{forecast.list[0].dt_txt}</div>
-                  <div className="">
-                    <img src={`https://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png`}
-                            alt={forecast.list[0].weather[0].description}
-                            width={64}
-                            height={64}
-                            />
-                      <div className="capitalize text-gray-500 mb-1">{forecast.list[0].weather[0].description}</div>
+                  <div className="text-center font-bold text-2xl mb-6">Looking ahead...</div>
+                  <div className="mb-2 border-b border-gray-500">
+                    <div className="font-semibold">{forecast.list[0].dt_txt}</div>
+                    <div className="">
+                      <img src={`https://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png`}
+                              alt={forecast.list[0].weather[0].description}
+                              width={64}
+                              height={64}
+                              />
+                        <div className="capitalize text-gray-500 mb-1">{forecast.list[0].weather[0].description}</div>
+                    </div>
                   </div>
-                </div>
-                
                   
+                    
 
-                <div className="mb-2 border-b border-gray-500">
-                  <div className="font-semibold">{forecast.list[1].dt_txt}</div>
-                  <div className="">
-                    <img src={`https://openweathermap.org/img/wn/${forecast.list[1].weather[0].icon}@2x.png`}
-                            alt={forecast.list[1].weather[0].description}
-                            width={64}
-                            height={64}
-                            />
-                      <div className="capitalize text-gray-500 mb-1">{forecast.list[1].weather[0].description}</div>
+                  <div className="mb-2 border-b border-gray-500">
+                    <div className="font-semibold">{forecast.list[1].dt_txt}</div>
+                    <div className="">
+                      <img src={`https://openweathermap.org/img/wn/${forecast.list[1].weather[0].icon}@2x.png`}
+                              alt={forecast.list[1].weather[0].description}
+                              width={64}
+                              height={64}
+                              />
+                        <div className="capitalize text-gray-500 mb-1">{forecast.list[1].weather[0].description}</div>
+                    </div>
                   </div>
-                </div>
 
-                <div className="mb-2 border-b border-gray-500">
-                  <div className="font-semibold">{forecast.list[2].dt_txt}</div>
-                  <div className="">
-                    <img src={`https://openweathermap.org/img/wn/${forecast.list[2].weather[0].icon}@2x.png`}
-                            alt={forecast.list[2].weather[0].description}
-                            width={64}
-                            height={64}
-                            />
-                      <div className="capitalize text-gray-500 mb-1">{forecast.list[2].weather[0].description}</div>
+                  <div className="mb-2 border-b border-gray-500">
+                    <div className="font-semibold">{forecast.list[2].dt_txt}</div>
+                    <div className="">
+                      <img src={`https://openweathermap.org/img/wn/${forecast.list[2].weather[0].icon}@2x.png`}
+                              alt={forecast.list[2].weather[0].description}
+                              width={64}
+                              height={64}
+                              />
+                        <div className="capitalize text-gray-500 mb-1">{forecast.list[2].weather[0].description}</div>
+                    </div>
                   </div>
-                </div>
 
-                  </CardContent>
-              </Card>
-            </div>
+                    </CardContent>
+                </Card>
+              </motion.div>
+            </motion.div>
           )}
         </div>
       </div>
